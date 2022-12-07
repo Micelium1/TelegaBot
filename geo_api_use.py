20d052e1f98e18c4170368ffd5e37d3bdc79769d
Micelium <maks.237@mail.ru>
2022-12-07 22:21:51 +0300

    Fix

--- geo_api_use.py
+++ geo_api_use.py
@@ -15,8 +15,9 @@ def get_coords(user_input:str) -> Coords:
     geo_response = get_geo_response(
         city_name = user_input
     )
-    weather = _parse_geo_response(geo_response)
-    return weather
+    coords = _parse_geo_response(geo_response)
+    print(coords.longitude,coords.latitude)
+    return coords
 
 
 def get_geo_response(city_name:str) -> str:
@@ -24,16 +25,16 @@ def get_geo_response(city_name:str) -> str:
     return urlopen(url).read()
 
 
-def _parse_longitude(openweather_dict: dict) -> str:
-    return openweather_dict[0]["lon"]
+def _parse_longitude(coord_dict: dict) -> str:
+    return coord_dict[0]["lon"]
 
 
-def _parse_latitude(openweather_dict: dict) -> str:
-    return openweather_dict[0]["lat"]
+def _parse_latitude(coord_dict: dict) -> str:
+    return coord_dict[0]["lat"]
 
 
 def _parse_geo_response(geo_response: str) -> Coords:
-    openweather_dict = json.loads(geo_response)
+    coord_dict = json.loads(geo_response)
     return Coords(
-        longitude=float(_parse_longitude(openweather_dict)),latitude=float(_parse_latitude(openweather_dict))
+        longitude=float(_parse_longitude(coord_dict)),latitude=float(_parse_latitude(coord_dict))
     )
