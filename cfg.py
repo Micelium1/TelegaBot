1fcfe712ccf892c700860b2750fbae0d4935a4b5
Micelium <maks.237@mail.ru>
2022-12-07 22:27:22 +0300

    Fix2

--- cfg.py
+++ cfg.py
@@ -1,8 +1,8 @@
-open_key = ''
+open_key = 'e8e0210a17ad4144fbb46748a8ed53f5'
 
-bot_key = ''
+bot_key = '5906611531:AAHcj5BxK_CDuLdYhjSJH9HHZST1I3mxZEM'
 
-weather_api = 'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longtitude}&appid=' + open_key + '&units=metric&lang=RU'
+weather_api = 'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=' + open_key + '&units=metric&lang=RU'
 
 geo_api = 'http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid=' + open_key
 
